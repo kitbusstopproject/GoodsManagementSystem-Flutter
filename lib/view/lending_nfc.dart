3e9cc87e66863a8b8042a42a3a06ca0d8750ac2c
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-07-27 17:37:40 +0900

    パッケージ名、アプリアイコン変更

--- lib/view/lending_nfc.dart
+++ lib/view/lending_nfc.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:goodsmanagementsystem/firebase/getFireStore.dart';
 import 'package:goodsmanagementsystem/view/lending_nfc_input.dart';
 import 'package:nfc_manager/nfc_manager.dart';
 
