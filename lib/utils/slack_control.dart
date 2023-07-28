878a41dcc53d6d25c3d29cdff596fa0ba57e599e
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-07-28 13:12:44 +0900

    メッセージにずんだ問のアイコンを追加

--- lib/utils/slack_control.dart
+++ lib/utils/slack_control.dart
@@ -7,22 +7,20 @@ import "env.dart";
 
 class SlackControl {
   static lendingInformationSend(String name, String item) {
-    final slack =
-        SlackNotifier(webHookUrl);
+    final slack = SlackNotifier(webHookUrl);
     slack.send(
       '物品の貸し出しがありました',
       channel: 'flutterアプリテストチャンネル',
       iconEmoji: ':chart_with_upwards_trend:',
       iconUrl: 'https://picsum.photos/48/48',
       username: 'My Bot',
-      blocks: [SectionBlock(text: '物品の貸し出しがありました')],
+      blocks: [SectionBlock(text: '物品の貸し出しがありました:zundamon:')],
       attachments: [Attachment(pretext: '$nameさん', text: '物品名:$item')],
     );
   }
 
   static returnInformationSend(String lenderName, String item) {
-    final slack =
-        SlackNotifier(webHookUrl);
+    final slack = SlackNotifier(webHookUrl);
     slack.send(
       '貸し出されていた物品が返却されました',
       channel: 'flutterアプリテストチャンネル',
