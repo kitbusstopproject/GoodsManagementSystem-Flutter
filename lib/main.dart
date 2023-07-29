5cc19f92f616d33317061f420ec34c783fb24502
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-07-29 11:59:26 +0900

    edgeToEdge

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:goodsmanagementsystem/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:goodsmanagementsystem/firebase/get_firestore.dart';
@@ -18,7 +19,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent,
+      ),
+    );
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.edgeToEdge,
+    );
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
       title: 'GoodsManagementSystemApp',
       theme: ThemeData(
